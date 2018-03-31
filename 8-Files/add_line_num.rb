c941fac317ee6433b68d2ec7e1ca90d11be3eb63
Jesen Tanadi <jesen@Jesens-MBP.home>
2018-03-31 17:05:38 -0400

    move texts into their own directory

--- 8-Files/add_line_num.rb
+++ 8-Files/add_line_num.rb
@@ -1,8 +1,8 @@
-File.open("simple_file.txt", "r") do |read_file|
+File.open("texts/simple_file.txt", "r") do |read_file|
   counter = 1
   
   read_file.readlines.each do |line|
-    File.open("simple_file_nums.txt", "a") do |write_file|
+    File.open("texts/simple_file_nums.txt", "a") do |write_file|
       # Format each line to start with num & end with period + newline:
       # 1. Lorem ipsum bla bla.\n
       new_line = line[-2] != "." ? "#{line[0..-2]}.\n" : "#{line}"
--- 8-Files/exercise1.rb
+++ 8-Files/exercise1.rb
@@ -1,10 +1,10 @@
 # Read file as 1 string & assign to variable "simple"
-simple = File.read("simple_file.txt")
+simple = File.read("texts/simple_file.txt")
 
 # Open file in append mode
 # (if file doesn't exist, it will be created)
 # Using a block will close the file at the end
-File.open("new_file.txt", "a") do |file|
+File.open("texts/new_file.txt", "a") do |file|
   # Method 1 of writing to file (shovel)
   file << simple
 
